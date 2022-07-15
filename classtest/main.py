9e37f5432edca1535a0cb4d851f4e202d5c58d8a
SujinRoh <suuz.hha@gmail.com>
2022-07-15 22:53:07 +0900

    move folder and class study

---
+++ classtest/main.py
@@ -0,0 +1,21 @@
+class Car():
+    
+    def __init__(self, *args, **kwargs):
+        self.wheels = 4
+        self.doors = 4
+        self.windows = 4
+        self.color = kwargs.get("color", "black")
+        self.price = kwargs.get("price", "$20")
+    
+    def __str__(self):
+       return f"Car with {self.wheels} wheels"
+       
+        
+        
+porche = Car(color="green", price="$40")
+print(porche.color, porche.price)
+# print(dir(Car))
+
+
+mini = Car()
+print(mini.color, mini.price)
